37f94d76184badae30033d36019f1cd5e8e3f55b
yeongipark <122340316+yeongipark@users.noreply.github.com>
2025-07-14 16:30:01 +0900

    chore: eslint, prettier 설정 변경 (#13)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,32 +6,39 @@ import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettierConfig,
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        plugins: {
-            '@typescript-eslint': tseslint.plugin,
-            react,
-            'react-hooks': reactHooks,
-            prettier: prettierPlugin,
-        },
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: {
-                project: './tsconfig.json',
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-        },
-        rules: {
-            'react/react-in-jsx-scope': 'off',
-            'prettier/prettier': 'error',
-            quotes: ['error', 'single', { avoidEscape: true }],
-        },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react,
+      'react-hooks': reactHooks,
+      prettier: prettierPlugin,
     },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'prettier/prettier': [
+        'error',
+        {
+          jsxSingleQuote: true,
+          singleQuote: true,
+          semi: true,
+        },
+      ],
+    },
+  },
 ];
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,3 @@
-import Hi from '@/component/Hi';
-
 export default function App() {
-  return (
-    <h1>
-      <Hi />
-    </h1>
-  );
+  return;
 }
