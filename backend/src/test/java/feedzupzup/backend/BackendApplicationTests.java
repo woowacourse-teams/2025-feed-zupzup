110139efdf8c33ca759cde884a160922c75dc4a6
Yeongjae Kim <sees77770@gmail.com>
2025-07-15 17:40:42 +0900

    [BE] chore: 테스트 컨테이너 환경 설정 (#37)
    * chore: gradle 테스트 설정
    
    * chore: testcontainers 의존성 추가 및 설정

--- backend/src/test/java/feedzupzup/backend/BackendApplicationTests.java
+++ backend/src/test/java/feedzupzup/backend/BackendApplicationTests.java
@@ -1,9 +1,11 @@
 package feedzupzup.backend;
 
+import feedzupzup.backend.config.TestcontainersTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestcontainersTest
 class BackendApplicationTests {
 
     @Test
