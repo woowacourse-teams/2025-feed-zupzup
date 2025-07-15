f51101c4f69c8791c5281c95ed9c0e58f8959a8c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-15 15:43:41 +0900

    feat: 피드백 엔티티 구현

---
+++ backend/src/main/java/feedzupzup/backend/feedback/domain/Feedback.java
@@ -0,0 +1,31 @@
+package feedzupzup.backend.feedback.domain;
+
+import feedzupzup.backend.global.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feedback extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private boolean isSecret;
+
+    @Enumerated(EnumType.STRING)
+    private ProcessStatus status;
+
+    private Long placeId;
+}
