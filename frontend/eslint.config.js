1a794fffbe7499a9854c44f237ed6ecd45d84d08
bunju20 <ghkd2271@naver.com>
2025-07-16 14:56:44 +0900

    chore: Storybook 설정 및 예시 컴포넌트 추가

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
@@ -5,40 +8,35 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettierConfig,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react,
-      'react-hooks': reactHooks,
-      prettier: prettierPlugin,
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
+export default [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig, {
+  files: ['**/*.ts', '**/*.tsx'],
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    react,
+    'react-hooks': reactHooks,
+    prettier: prettierPlugin,
+  },
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: './tsconfig.json',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'prettier/prettier': [
-        'error',
-        {
-          jsxSingleQuote: true,
-          singleQuote: true,
-          semi: true,
-        },
-      ],
-    },
   },
-];
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        jsxSingleQuote: true,
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+  },
+}, ...storybook.configs["flat/recommended"]];
