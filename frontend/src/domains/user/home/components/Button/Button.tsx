54208112cda34be776b0067d801b8af2d5761ba4
yeongipark <yeongi0111@naver.com>
2025-07-16 16:21:34 +0900

    chore: stylelint 설정

--- frontend/src/domains/user/home/components/Button/Button.tsx
+++ frontend/src/domains/user/home/components/Button/Button.tsx
@@ -1,3 +1,16 @@
+import { css } from '@emotion/react';
+
 export default function LikeButton() {
-  return <div>hello</div>;
+  return (
+    <div
+      css={css`
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        background-color: aliceblue;
+      `}
+    >
+      hello
+    </div>
+  );
 }
