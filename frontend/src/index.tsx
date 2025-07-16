37762ee970322dad93c7eb6d0d284f486b74ff17
yeongipark <yeongi0111@naver.com>
2025-07-16 14:55:17 +0900

    chore: themeProvider 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,12 @@ import './reset.css';
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './theme';
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
