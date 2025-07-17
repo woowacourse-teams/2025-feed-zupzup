363c4bbbbe684f23663297ee6577c1fb8e880ba8
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-17 20:18:56 +0900

    refactor: `@ResponseStatus` 애노테이션 제거

--- backend/src/main/java/feedzupzup/backend/feedback/api/UserFeedbackApi.java
+++ backend/src/main/java/feedzupzup/backend/feedback/api/UserFeedbackApi.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Tag(name = "User Feedback", description = "피드백 API(사용자 권한)")
 @RequestMapping("/api")
@@ -31,7 +29,6 @@ public interface UserFeedbackApi {
             @ApiResponse(responseCode = "200", description = "조회 성공", useReturnTypeSchema = true),
             @ApiResponse(responseCode = "404", ref = "#/components/responses/NotFound")
     })
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/places/{placeId}/feedbacks")
     SuccessResponse<UserFeedbackListResponse> getUserFeedbacks(
             @Parameter(description = "장소 ID", example = "1") @PathVariable("placeId") final Long placeId,
@@ -45,7 +42,6 @@ public interface UserFeedbackApi {
             @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
             @ApiResponse(responseCode = "404", ref = "#/components/responses/NotFound")
     })
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/places/{placeId}/feedbacks")
     SuccessResponse<CreateFeedbackResponse> create(
             @Parameter(description = "장소 ID", example = "1") @PathVariable("placeId") final Long placeId,
