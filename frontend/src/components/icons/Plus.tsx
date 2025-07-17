a14e36df1c78feda44a4330de57746cc29290224
bunju20 <85238126+bunju20@users.noreply.github.com>
2025-07-17 13:35:04 +0900

    [FE] feat: BasicButton 컴포넌트 구현 및 hero컴포넌트 세팅  (#71)
    * feat: BasicButton 구현
    - 공통컴포넌트 일부 수정
    
    * feat: Hero 컴포넌트 이미지 파일 추가
    - styles.d.ts에 이미지 파일 모듈 선언 추가
    - Hero 컴포넌트에 배경 이미지 및 스타일 적용
    
    * feat: BasicButton secondary 속성 추가
    
    * feat: 경로 별칭을 사용하여 import 경로 수정
    
    * chore: BasicButton 및 Hero 컴포넌트 스타일 수정
    - BasicButton 스타일 함수 이름 변경
    - Hero 컴포넌트 스타일 이름 변경
    
    * style: BasicButton 텍스트 스타일 간소화

---
+++ frontend/src/components/icons/Plus.tsx
@@ -0,0 +1,26 @@
+export default function Plus() {
+  return (
+    <svg
+      width='15'
+      height='14'
+      viewBox='0 0 15 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M3.4165 7H11.5832'
+        stroke='black'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M7.5 2.9165V11.0832'
+        stroke='black'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
