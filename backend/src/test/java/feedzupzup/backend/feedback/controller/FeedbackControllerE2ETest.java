65b593f78a73fa25be3adf0569cf1a1cb7eef234
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-17 11:05:26 +0900

    test: api e2e 테스트 작성

---
+++ backend/src/test/java/feedzupzup/backend/feedback/controller/FeedbackControllerE2ETest.java
@@ -0,0 +1,47 @@
+package feedzupzup.backend.feedback.controller;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import feedzupzup.backend.config.TestcontainersTest;
+import feedzupzup.backend.feedback.dto.request.CreateFeedbackRequest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestcontainersTest
+class FeedbackControllerE2ETest extends E2EHelper {
+
+    @Test
+    @DisplayName("피드백을 성공적으로 생성한다")
+    void create_secret_feedback_success() {
+        // given
+        final Long placeId = 1L;
+        final CreateFeedbackRequest request = new CreateFeedbackRequest(
+                "개선이 필요해요",
+                "https://example.com/image.jpg",
+                true
+        );
+
+        // when & then
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/places/{placeId}/feedbacks", placeId)
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .contentType(ContentType.JSON)
+                .body("status", equalTo(201))
+                .body("message", equalTo("CREATED"))
+                .body("data.feedbackId", notNullValue())
+                .body("data.content", equalTo("개선이 필요해요"))
+                .body("data.isSecret", equalTo(true))
+                .body("data.createdAt", notNullValue());
+    }
+}
