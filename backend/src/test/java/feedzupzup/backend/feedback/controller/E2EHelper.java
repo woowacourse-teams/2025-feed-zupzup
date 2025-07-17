ca343323ffc5c79f6b20c3feae04358ec4efa27a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-17 11:03:30 +0900

    test: 테스트 헬퍼 클래스 구현

---
+++ backend/src/test/java/feedzupzup/backend/feedback/controller/E2EHelper.java
@@ -0,0 +1,27 @@
+package feedzupzup.backend.feedback.controller;
+
+import feedzupzup.backend.config.DataInitializer;
+import feedzupzup.backend.config.TestcontainersTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestcontainersTest
+public abstract class E2EHelper {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private DataInitializer dataInitializer;
+
+    @BeforeEach
+    void setUp() {
+        dataInitializer.deleteAll();
+        RestAssured.port = port;
+    }
+}
