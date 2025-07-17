a4a6886dd8b58ac49cdd81ac7c34a5df9d29d02c
mun-kyeong <horse6953@naver.com>
2025-07-17 14:26:58 +0900

    feat: ArrowLeftButton 컴포넌트 및 타입 추가

---
+++ frontend/src/components/ArrowLeftButton/ArrowLeft.tsx
@@ -0,0 +1,30 @@
+import { ArrowLeftProps } from './ArrowLeft.type';
+
+export default function ArrowLeft({ onClick }: ArrowLeftProps) {
+  return (
+    <button onClick={onClick}>
+      <svg
+        width='14'
+        height='15'
+        viewBox='0 0 14 15'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M6.99984 11.5832L2.9165 7.49984L6.99984 3.4165'
+          stroke='#4A5565'
+          stroke-width='1.16667'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+        <path
+          d='M11.0832 7.5H2.9165'
+          stroke='#4A5565'
+          stroke-width='1.16667'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+      </svg>
+    </button>
+  );
+}
---
+++ frontend/src/components/ArrowLeftButton/ArrowLeft.type.ts
@@ -0,0 +1,3 @@
+export interface ArrowLeftProps extends React.ComponentProps<'button'> {
+  onClick: () => void;
+}
