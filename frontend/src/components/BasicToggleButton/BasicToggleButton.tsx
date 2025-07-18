d8549f07b9781623696d8c8254503eb5b7649dc4
mun-kyeong <horse6953@naver.com>
2025-07-18 17:01:16 +0900

    feat: BasicToggleButton 스타일 리팩토링

--- frontend/src/components/BasicToggleButton/BasicToggleButton.tsx
+++ frontend/src/components/BasicToggleButton/BasicToggleButton.tsx
@@ -1,6 +1,9 @@
 import { useAppTheme } from '@/hooks/useAppTheme';
-import { Theme } from '@/theme';
-import { css } from '@emotion/react';
+import {
+  circleStyle,
+  toggleWrapper,
+  trackStyle,
+} from './BasicToggleButton.style';
 
 interface ToggleButtonProps {
   isOn: boolean;
@@ -14,44 +17,10 @@ export default function BasicToggleButton({
   const theme = useAppTheme();
   return (
     <>
-      <div css={toggleContainer(theme)} onClick={onClick}>
-        <div
-          className={`toggle-container ${isOn ? 'toggle--checked' : null}`}
-        />
-        <div className={`toggle-circle ${isOn ? 'toggle--checked' : null}`} />
+      <div css={toggleWrapper} onClick={onClick}>
+        <div css={trackStyle(theme, isOn)} />
+        <div css={circleStyle(isOn)} />
       </div>
     </>
   );
 }
-
-const toggleContainer = (theme: Theme) => css`
-  position: relative;
-  cursor: pointer;
-
-  > .toggle-container {
-    width: 50px;
-    height: 24px;
-    border-radius: 30px;
-    background-color: rgb(224, 224, 224);
-  }
-
-  > .toggle--checked {
-    background-color: ${theme.colors.yellow[200]};
-    transition: 0.5s;
-  }
-
-  > .toggle-circle {
-    position: absolute;
-    top: 2px;
-    left: 1px;
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    background-color: rgb(255, 254, 255);
-    transition: 0.5s;
-  }
-  > .toggle--checked {
-    left: 27px;
-    transition: 0.5s;
-  }
-`;
