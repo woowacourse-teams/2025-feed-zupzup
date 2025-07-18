044d7460ebe065879f6c4ebe7b8fe3d0c3be2659
yeongipark <yeongi0111@naver.com>
2025-07-18 14:39:07 +0900

    style: 완료된 피드백 텍스트 색깔 변경

--- frontend/src/components/FeedbackText/FeedbackText.tsx
+++ frontend/src/components/FeedbackText/FeedbackText.tsx
@@ -3,10 +3,11 @@ import { feedbackText } from './FeedbackText.styles';
 
 export interface FeedbackTextProps {
   text: string;
+  type: 'incomplete' | 'complete';
 }
 
-export default function FeedbackText({ text }: FeedbackTextProps) {
+export default function FeedbackText({ text, type }: FeedbackTextProps) {
   const theme = useAppTheme();
 
-  return <p css={feedbackText(theme)}>{text}</p>;
+  return <p css={feedbackText(theme, type)}>{text}</p>;
 }
