8361ee57bed9c0876ed13800504744a7ec80dd6c
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-07-18 13:08:53 +0900

    refactor: 공통 컴포넌트의 타입 정의 통합 및 불필요한 파일 제거 (#83)

--- frontend/src/components/@commons/Button/Button.types.ts
+++
@@ -1,6 +0,0 @@
-import { SerializedStyles } from '@emotion/react';
-
-export interface ButtonProps extends React.ComponentProps<'button'> {
-  children: React.ReactNode;
-  customCSS?: SerializedStyles | SerializedStyles[];
-}
