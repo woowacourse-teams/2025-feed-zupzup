ecf98ecf145b16de305c8b94f076317a8fa7e8ca
yeongipark <yeongi0111@naver.com>
2025-07-18 12:52:27 +0900

    chore: UserFeedbackBox에 필요한 아이콘 설정

--- frontend/src/components/icons/EmptyHeart.tsx
+++ frontend/src/components/icons/EmptyHeart.tsx
@@ -1,9 +1,9 @@
 export default function EmptyHeart() {
   return (
     <svg
-      width='12'
+      width='14'
       height='12'
-      viewBox='0 0 12 12'
+      viewBox='0 0 14 12'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
