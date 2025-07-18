4770b7ddf7b40bdd728ff6f24b9bf0d7a3571ee0
mun-kyeong <horse6953@naver.com>
2025-07-18 12:59:31 +0900

    refactor: 공통 컴포넌트의 타입 정의 통합 및 불필요한 파일 제거

--- frontend/src/components/@commons/Input/Input.tsx
+++ frontend/src/components/@commons/Input/Input.tsx
@@ -1,4 +1,13 @@
-import { InputProps } from './Input.types';
+import { SerializedStyles } from '@emotion/react';
+
+export interface InputProps extends React.ComponentProps<'input'> {
+  value: string;
+  onChange: () => void;
+  placeholder: string;
+  css: SerializedStyles;
+  maxLength: number;
+  minLength: number;
+}
 
 export default function Input({
   value,
--- frontend/src/components/@commons/TextArea/TextArea.tsx
+++ frontend/src/components/@commons/TextArea/TextArea.tsx
@@ -1,4 +1,13 @@
-import { TextAreaProps } from './TextArea.types';
+import { SerializedStyles } from '@emotion/react';
+
+export interface TextAreaProps extends React.ComponentProps<'textarea'> {
+  value: string;
+  onChange: () => void;
+  placeholder: string;
+  css: SerializedStyles;
+  maxLength: number;
+  minLength: number;
+}
 
 export default function TextArea({
   value,
