327591aee586c30aab865f0c09b165bc190df66c
yeongipark <yeongi0111@naver.com>
2025-07-20 16:56:58 +0900

    chore: 관리자 홈 화면 라우팅 연결

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,18 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Suggestions from './domains/user/suggestions/Suggestions';
-import App from './App';
+import AdminHome from './domains/admin/home/AdminHome';
+import UserHome from './domains/user/home/UserHome';
 
 export const router = createBrowserRouter([
   {
-    path: '/root',
-    element: <App />,
+    path: '/',
+    element: <UserHome />,
   },
   {
     path: '/suggestion',
     element: <Suggestions />,
   },
-  // {
-  //   path: '/admin',
-  //   element: <App />,
-  // },
+  {
+    path: '/admin',
+    element: <AdminHome />,
+  },
 ]);
