967b2ff627cd2d017face06c0127614c87cdc84c
yeongipark <yeongi0111@naver.com>
2025-07-20 17:32:30 +0900

    test: StatusTag 스토리북 작성

--- frontend/src/domains/components/StatusTag/StatusTag.stories.ts
+++ frontend/src/domains/components/StatusTag/StatusTag.stories.ts
@@ -14,10 +14,6 @@ const meta: Meta<typeof StatusTag> = {
       options: ['incomplete', 'complete'],
       description: '상태에 따라 태그 스타일이 달라집니다.',
     },
-    children: {
-      control: 'text',
-      description: '태그 내부에 표시할 텍스트입니다.',
-    },
   },
 };
 
@@ -26,14 +22,12 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: '미완료',
     type: 'incomplete',
   },
 };
 
 export const Complete: Story = {
   args: {
-    children: '완료',
     type: 'complete',
   },
 };
