8b4208fe1bd117a15929e9f21c9a84847eb84537
mun-kyeong <horse6953@naver.com>
2025-07-20 19:00:58 +0900

    fix: SVG 속성의 대문자 표기법으로 수정

--- frontend/src/components/icons/ArrowLeft.tsx
+++ frontend/src/components/icons/ArrowLeft.tsx
@@ -10,16 +10,16 @@ export default function ArrowLeft() {
       <path
         d='M6.99984 11.5832L2.9165 7.49984L6.99984 3.4165'
         stroke='#4A5565'
-        stroke-width='1.16667'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M11.0832 7.5H2.9165'
         stroke='#4A5565'
-        stroke-width='1.16667'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
--- frontend/src/components/icons/lock.tsx
+++ frontend/src/components/icons/lock.tsx
@@ -10,16 +10,16 @@ export default function Lock() {
       <path
         d='M11.5833 6.6665H3.41667C2.77233 6.6665 2.25 7.18884 2.25 7.83317V11.9165C2.25 12.5608 2.77233 13.0832 3.41667 13.0832H11.5833C12.2277 13.0832 12.75 12.5608 12.75 11.9165V7.83317C12.75 7.18884 12.2277 6.6665 11.5833 6.6665Z'
         stroke='#364153'
-        stroke-width='1.16667'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M4.5835 6.6665V4.33317C4.5835 3.55962 4.89079 2.81776 5.43777 2.27078C5.98475 1.72379 6.72661 1.4165 7.50016 1.4165C8.27371 1.4165 9.01558 1.72379 9.56256 2.27078C10.1095 2.81776 10.4168 3.55962 10.4168 4.33317V6.6665'
         stroke='#364153'
-        stroke-width='1.16667'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
