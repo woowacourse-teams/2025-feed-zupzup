b07b774cb4067e50db279226a3e5f3ea8a385f33
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-22 22:14:07 +0900

    refactor: Long 원시 타입 참조 타입으로 수정

--- backend/src/main/java/feedzupzup/backend/feedback/domain/FeedbackLikeInMemoryRepository.java
+++ backend/src/main/java/feedzupzup/backend/feedback/domain/FeedbackLikeInMemoryRepository.java
@@ -12,12 +12,12 @@ public class FeedbackLikeInMemoryRepository {
 
     private final ConcurrentHashMap<Long, AtomicInteger> feedbackLikes = new ConcurrentHashMap<>();
 
-    public int increase(final long feedbackId) {
+    public int increase(final Long feedbackId) {
         return feedbackLikes.computeIfAbsent(feedbackId, id -> new AtomicInteger(0))
                 .incrementAndGet();
     }
 
-    public int decrease(final long feedbackId) {
+    public int decrease(final Long feedbackId) {
         final AtomicInteger likeCount = feedbackLikes.get(feedbackId);
         if (likeCount == null) {
             throw new ResourceNotFoundException("좋아요 횟수는 음수가 될 수 없습니다.");
@@ -25,7 +25,7 @@ public class FeedbackLikeInMemoryRepository {
         return likeCount.decrementAndGet();
     }
 
-    public int get(final long feedbackId) {
+    public int get(final Long feedbackId) {
         final AtomicInteger likeCount = feedbackLikes.get(feedbackId);
         return likeCount != null ? likeCount.get() : 0;
     }
