3ac15968d8cb8fa4c0ad41a11e2aec87ca729852
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-22 15:44:13 +0900

    refactor: 장소가 없는 경우 예외를 던지게 수정

--- backend/src/main/java/feedzupzup/backend/place/domain/Place.java
+++ backend/src/main/java/feedzupzup/backend/place/domain/Place.java
@@ -1,13 +1,15 @@
-package feedzupzup.backend.place;
+package feedzupzup.backend.place.domain;
 
 import feedzupzup.backend.global.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Place extends BaseTimeEntity {
 
---
+++ backend/src/main/java/feedzupzup/backend/place/domain/PlaceRepository.java
@@ -0,0 +1,7 @@
+package feedzupzup.backend.place.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+}
