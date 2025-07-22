3921c3b185f55db87ec5ce3171e274399c2e8f92
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-07-22 23:59:25 +0900

    refactor: 스케쥴러에 트랜잭셔널 애노테이션 제거

--- backend/src/main/java/feedzupzup/backend/feedback/application/scheduler/FeedbackLikeScheduler.java
+++ backend/src/main/java/feedzupzup/backend/feedback/application/scheduler/FeedbackLikeScheduler.java
@@ -1,13 +1,10 @@
 package feedzupzup.backend.feedback.application.scheduler;
 
-import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
-
 import feedzupzup.backend.feedback.application.FeedbackLikeCounter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +13,6 @@ public class FeedbackLikeScheduler {
     private final FeedbackLikeCounter feedbackLikeCounter;
 
     @Async
-    @Transactional(propagation = REQUIRES_NEW)
     @Scheduled(cron = "0 0 * * * *")
     public void syncFeedbackLikeCounter() {
         feedbackLikeCounter.syncLikesToDatabase();
