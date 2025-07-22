032691fab066a0a5d03a2c929ff8501f8a798427
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-07-22 14:59:57 +0900

    [FE] fix: SVG icon 속성에서 스네이크 변경 -> 카멜 케이스로 변경 (#143)
    * fix: stroke-width 속성 수정
    
    * fix:  stroke-linecap 속성 변경
    
    * fix: stroke-linejoin 속성 변경
    
    * fix: fillOpacity 속성 변경

--- frontend/src/components/icons/CalendarIcon.tsx
+++ frontend/src/components/icons/CalendarIcon.tsx
@@ -10,30 +10,30 @@ export default function CalendarIcon() {
       <path
         d='M3.5 1.09473V2.84473'
         stroke='#7C7C7C'
-        stroke-width='0.875'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='0.875'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M7 1.09473V2.84473'
         stroke='#7C7C7C'
-        stroke-width='0.875'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='0.875'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M8.3125 1.96973H2.1875C1.70425 1.96973 1.3125 2.36148 1.3125 2.84473V8.96973C1.3125 9.45298 1.70425 9.84473 2.1875 9.84473H8.3125C8.79575 9.84473 9.1875 9.45298 9.1875 8.96973V2.84473C9.1875 2.36148 8.79575 1.96973 8.3125 1.96973Z'
         stroke='#7C7C7C'
-        stroke-width='0.875'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='0.875'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M1.3125 4.59473H9.1875'
         stroke='#7C7C7C'
-        stroke-width='0.875'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='0.875'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
--- frontend/src/components/icons/CheckIcon.tsx
+++ frontend/src/components/icons/CheckIcon.tsx
@@ -10,9 +10,9 @@ export default function CheckIcon() {
       <path
         d='M11.6668 3.5L5.25016 9.91667L2.3335 7'
         stroke='#006045'
-        stroke-width='1.16667'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
