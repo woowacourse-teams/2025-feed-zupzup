06bf82cc990f70a302b1510545c43bce1421bac5
yeongipark <yeongi0111@naver.com>
2025-07-22 17:13:59 +0900

    type: AdminFeedback 타입 설정

--- frontend/src/types/feedback.types.ts
+++ frontend/src/types/feedback.types.ts
@@ -1,14 +1,20 @@
-export interface Feedback {
+export interface UserFeedback {
   feedbackId: number;
   content: string;
   status: 'WAITING' | 'CONFIRMED';
   isSecret: boolean;
   isLiked: boolean;
   createdAt: string; // ISO 형식 날짜 문자열
+  userName: string;
+}
+
+export interface AdminFeedback extends Omit<UserFeedback, 'isLiked'> {
+  imgUrl: string | null;
+  likeCount: number;
 }
 
 export interface FeedbackResponse {
-  feedbacks: Feedback[];
+  feedbacks: UserFeedback[];
   hasNext: boolean;
   nextCursorId: number;
 }
