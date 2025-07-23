f08bdc70cd9176e8062b2e7ca04d20acfb56307f
Yeongjae Kim <sees77770@gmail.com>
2025-07-23 15:47:19 +0900

    chore: s3 presigner 빈 등록

---
+++ backend/src/main/java/feedzupzup/backend/s3/config/S3Config.java
@@ -0,0 +1,23 @@
+package feedzupzup.backend.s3.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Slf4j
+@Configuration
+public class S3Config {
+
+    @Value("${s3.region}")
+    private String region;
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .build();
+    }
+}
