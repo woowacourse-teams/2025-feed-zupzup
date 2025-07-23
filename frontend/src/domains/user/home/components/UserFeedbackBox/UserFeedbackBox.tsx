b5b94eaff5e603b42be5394b20b66cbe361e66aa
yeongipark <yeongi0111@naver.com>
2025-07-23 15:11:14 +0900

    feat: LockIcon 추가 및 비밀글 표시 개선

--- frontend/src/domains/user/home/components/UserFeedbackBox/UserFeedbackBox.tsx
+++ frontend/src/domains/user/home/components/UserFeedbackBox/UserFeedbackBox.tsx
@@ -1,10 +1,12 @@
 import CategoryTag from '../../../../components/CategoryTag/CategoryTag';
 import StatusTag from '../../../../components/StatusTag/StatusTag';
 import FeedbackText from '@/domains/components/FeedbackText/FeedbackText';
-import { tagContainer } from './UserFeedbackBox.styles';
+import { secretText, tagContainer } from './UserFeedbackBox.styles';
 import FeedbackBoxFooter from '@/domains/components/FeedbackBoxFooter/FeedbackBoxFooter';
 import FeedbackBoxBackGround from '@/domains/components/FeedbackBoxBackGround/FeedbackBoxBackGround';
 import { FeedbackStatusType } from '@/types/feedbackStatus.types';
+import LockIcon from '@/components/icons/LockIcon';
+import { useAppTheme } from '@/hooks/useAppTheme';
 
 interface UserFeedbackBox {
   type: FeedbackStatusType;
@@ -21,13 +23,24 @@ export default function UserFeedbackBox({
   isSecret,
   createdAt,
 }: UserFeedbackBox) {
+  const theme = useAppTheme();
+
   return (
     <FeedbackBoxBackGround type={type}>
       <div css={tagContainer}>
         <CategoryTag text='맛' type={type} />
         <StatusTag type={type} />
       </div>
-      {isSecret ? '비밀글입니다.' : <FeedbackText type={type} text={content} />}
+      {isSecret ? (
+        <p css={secretText(theme)}>
+          <p>비밀글입니다.</p>
+          <p>
+            <LockIcon />
+          </p>
+        </p>
+      ) : (
+        <FeedbackText type={type} text={content} />
+      )}
       <FeedbackBoxFooter isLiked={isLiked} createdAt={createdAt} />
     </FeedbackBoxBackGround>
   );
