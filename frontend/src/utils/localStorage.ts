042db97a28ed3a539c222b5546f1b003abd33c77
yeongipark <yeongi0111@naver.com>
2025-07-24 15:27:12 +0900

    feat: 로컬스토리지 로직 추가

---
+++ frontend/src/utils/localStorage.ts
@@ -0,0 +1,19 @@
+export function getLocalStorage<T = string>(key: string): T | null {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
+  try {
+    const data = localStorage.getItem(key);
+    if (!data) return [] as T;
+
+    return JSON.parse(data) as T;
+  } catch {
+    console.error(`${key}값이 존재하지 않습니다.`);
+    return null;
+  }
+}
+
+export function setLocalStorage<T = string>(key: string, dataList: T) {
+  localStorage.setItem(key, JSON.stringify(dataList));
+}
