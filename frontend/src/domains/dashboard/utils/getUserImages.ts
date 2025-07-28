be012e8b6d352f49fd8103fe4710868e7c3fc90a
mun-kyeong <horse6953@naver.com>
2025-07-28 20:27:22 +0900

    feat: 사용자 이미지 가져오기 유틸리티 추가

---
+++ frontend/src/domains/dashboard/utils/getUserImages.ts
@@ -0,0 +1,10 @@
+import IMG1 from '@/assets/images/user1.png';
+import IMG2 from '@/assets/images/user2.png';
+import IMG3 from '@/assets/images/user3.png';
+import IMG4 from '@/assets/images/user4.png';
+import IMG5 from '@/assets/images/user5.png';
+import IMG6 from '@/assets/images/user6.png';
+import IMG7 from '@/assets/images/user7.png';
+import IMG8 from '@/assets/images/user8.png';
+
+export const userImages = [IMG1, IMG2, IMG3, IMG4, IMG5, IMG6, IMG7, IMG8];
