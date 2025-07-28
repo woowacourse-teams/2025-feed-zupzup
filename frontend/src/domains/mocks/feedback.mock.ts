5b3370e603539f662fb0e55f279e3c4a0d543dba
mun-kyeong <horse6953@naver.com>
2025-07-29 00:48:42 +0900

    feat: 피드백 mock 데이터 공통 mock 파일로 이동

--- frontend/src/domains/mocks/feedback.mock.ts
+++ frontend/src/domains/mocks/feedback.mock.ts
@@ -1,8 +1,10 @@
+import { FeedbackStatusType } from '@/types/feedbackStatus.types';
+
 export const FEEDBACK_MOCK = [
   {
     userName: '김코딩',
     feedbackId: 1,
-    status: 'WAITING',
+    status: 'WAITING' as FeedbackStatusType,
     content: '다음에는 점심 메뉴를 다양하게 해주세요.',
     createdAt: '2024-06-01T10:00:00Z',
     isSecret: false,
@@ -11,7 +13,7 @@ export const FEEDBACK_MOCK = [
   {
     userName: '웨이브하다가허리부러진 웨이드',
     feedbackId: 2,
-    status: 'WAITING',
+    status: 'WAITING' as FeedbackStatusType,
     content: '회의실 예약 시스템이 불편해요.',
     createdAt: '2024-06-02T12:30:00Z',
     isSecret: true,
@@ -20,7 +22,7 @@ export const FEEDBACK_MOCK = [
   {
     userName: '웨이브하다가허리부러진 웨이드',
     feedbackId: 3,
-    status: 'COMPLETED',
+    status: 'COMPLETED' as FeedbackStatusType,
     content: '에어컨 온도를 조금 높여주세요.',
     createdAt: '2024-06-03T09:15:00Z',
     isSecret: false,
