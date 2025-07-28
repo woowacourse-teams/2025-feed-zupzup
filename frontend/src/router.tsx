3e42a9997d92e5329a5117aa0e9b54470d2ed394
bunju20 <ghkd2271@naver.com>
2025-07-28 19:39:04 +0900

    feat: 피드백 제출 기능 추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,16 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import Suggestions from './domains/user/suggestions/Suggestions';
 import AdminHome from './domains/admin/home/AdminHome';
 import UserHome from './domains/user/home/UserHome';
-import FeedbackInput from './domains/user/home/components/FeedbackInput/FeedbackInput';
 
 export const router = createBrowserRouter([
-  // {
-  //   path: '/',
-  //   element: <UserHome />,
-  // },
   {
     path: '/',
-    element: <FeedbackInput />,
+    element: <UserHome />,
   },
   {
     path: '/suggestion',
