3ca31cdb82dd09e458975ea0ed7ad5b1a7cf33d2
mun-kyeong <horse6953@naver.com>
2025-07-29 11:09:17 +0900

    feat: ClockIcon 컴포넌트 생성 및 경로 수정

--- frontend/src/domains/components/FeedbackBoxFooter/FeedbackBoxFooter.tsx
+++ frontend/src/domains/components/FeedbackBoxFooter/FeedbackBoxFooter.tsx
@@ -1,4 +1,4 @@
-import ClockIcon from '@/components/icons/CClockIcon';
+import ClockIcon from '@/components/icons/ClockIcon';
 import LikeButton from '@/domains/components/LikeButton/LikeButton';
 import { useAppTheme } from '@/hooks/useAppTheme';
 import {
