e734a4990a65894e33bd6ed4bbcbe8fddd20e1d5
mun-kyeong <horse6953@naver.com>
2025-07-29 21:35:34 +0900

    feat: Sentry 통합 및 Home 컴포넌트에 오류 발생 버튼 추가

--- frontend/src/domains/Home.tsx
+++ frontend/src/domains/Home.tsx
@@ -10,6 +10,13 @@ export default function Home() {
 
   return (
     <section css={container}>
+      <button
+        onClick={() => {
+          throw new Error('This is your first error!');
+        }}
+      >
+        Break the world
+      </button>
       <ProgressBar currentStep={currentStep} totalStep={totalStep} />
       {currentStep === 1 && <OnBoarding moveNextStep={moveNextStep} />}
     </section>
