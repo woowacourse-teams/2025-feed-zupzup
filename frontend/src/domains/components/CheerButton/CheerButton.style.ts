fb90ac170d4d0613c89351e2c420ba89acea81e5
mun-kyeong <horse6953@naver.com>
2025-07-29 11:40:06 +0900

    fix: border-radius를 50%로 수정하여 일관성 유지

--- frontend/src/domains/components/CheerButton/CheerButton.style.ts
+++ frontend/src/domains/components/CheerButton/CheerButton.style.ts
@@ -7,7 +7,7 @@ export const cheerButtonStyle = css`
   padding: 6px 12px;
   background-color: white;
   border: 1px solid #e0e0e0;
-  border-radius: 9999px;
+  border-radius: 50%;
 `;
 
 export const textStyle = css`
