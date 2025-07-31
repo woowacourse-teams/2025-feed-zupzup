0ae30bfd1a213697515261026ad0809edc9aa940
yeongipark <yeongi0111@naver.com>
2025-07-31 21:18:59 +0900

    feat: Alert과 Confirm 컴포넌트 스토리북 작성

---
+++ frontend/src/components/ConfirmModal/ConfirmModal.stories.tsx
@@ -0,0 +1,53 @@
+import ConfirmModal from '@/components/ConfirmModal/ConfirmModal';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useState } from 'react';
+
+const meta: Meta<typeof ConfirmModal> = {
+  title: 'components/ConfirmModal',
+  component: ConfirmModal,
+  tags: ['autodocs'],
+  argTypes: {
+    title: { control: 'text' },
+    message: { control: 'text' },
+    confirmText: { control: 'text' },
+    cancelText: { control: 'text' },
+    width: { control: 'number' },
+    height: { control: 'number' },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: '600px', height: '80vh' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ConfirmModal>;
+
+export const Default: Story = {
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    return (
+      <ConfirmModal
+        {...args}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={() => {
+          alert('확인 버튼이 클릭되었습니다.');
+          setIsOpen(false);
+        }}
+      />
+    );
+  },
+  args: {
+    title: '정말 삭제하시겠습니까?',
+    message: '삭제한 내용은 복구할 수 없습니다.',
+    confirmText: '삭제',
+    cancelText: '취소',
+    width: 360,
+  },
+};
--- frontend/src/components/Modal/Modal.styles.ts
+++ frontend/src/components/Modal/Modal.styles.ts
@@ -38,13 +38,13 @@ export const content = css`
 `;
 
 export const title = (theme: Theme) => css`
-  ${theme.typography.inter.h4};
+  ${theme.typography.BMHANNAPro.bodyBold};
 
   color: ${theme.colors.black[100]};
 `;
 
 export const message = (theme: Theme) => css`
-  ${theme.typography.inter.bodyRegular};
+  ${theme.typography.BMHANNAPro.small};
 
   color: ${theme.colors.gray[600]};
 `;
