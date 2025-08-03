8f6f7d7bbc99cdd364e7a78c67458347458a78fe
mun-kyeong <horse6953@naver.com>
2025-08-03 19:44:31 +0900

    feat: FloatingButton 컴포넌트에 inset 속성 추가 및 스타일 수정

--- frontend/src/domains/components/FloatingButton/FloatingButton.style.ts
+++ frontend/src/domains/components/FloatingButton/FloatingButton.style.ts
@@ -1,29 +1,15 @@
-import { Theme } from '@/theme';
 import { css } from '@emotion/react';
 
-export const floatingButton = (theme: Theme) => css`
+export const floatingButton = (inset: string[]) => css`
   position: sticky;
-  bottom: 32px;
-  left: 100%;
+  inset: ${inset.join(' ')};
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 14px;
-  width: 60px;
-  height: 60px;
-  background-color: ${theme.colors.purple[100]};
   border-radius: 50%;
   cursor: pointer;
   transition:
     bottom 0.3s ease,
     background-color 0.2s ease;
-
-  &:hover {
-    background-color: ${theme.colors.purple[100]}aa;
-  }
-`;
-
-export const floatingButtonIcon = css`
-  width: 16px;
-  height: 16px;
 `;
