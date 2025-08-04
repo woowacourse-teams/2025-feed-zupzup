87f34559e9a4693ff577ecb7447bfa3aa56f9f38
mun-kyeong <horse6953@naver.com>
2025-08-04 17:22:00 +0900

    [FE] chore: Sentry 초기화 코드 위치 변경 및 정리

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,20 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import './reset.css';
 import { theme } from './theme';
-<<<<<<< HEAD
-import { router } from './router';
-import * as Sentry from '@sentry/react';
-
-Sentry.init({
-  dsn: 'https://d078b25843b4ef88ed75b287a7ab8a4b@o4509750841245696.ingest.us.sentry.io/4509750843342849',
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 0.2,
-  sendDefaultPii: true,
-});
-=======
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
 import { ErrorModalProvider } from '@/contexts/useErrorModal';
+import * as Sentry from '@sentry/react';
 
 // if (process.env.NODE_ENV === 'development') {
 //   const { worker } = await import('./mocks/browser');
@@ -33,7 +23,13 @@ if ('serviceWorker' in navigator) {
       .catch((err) => console.log('Service Worker registration failed:', err));
   });
 }
->>>>>>> 2422b0bb708876a8cace7976931f4a12e03bfa94
+
+Sentry.init({
+  dsn: 'https://d078b25843b4ef88ed75b287a7ab8a4b@o4509750841245696.ingest.us.sentry.io/4509750843342849',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.2,
+  sendDefaultPii: true,
+});
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
