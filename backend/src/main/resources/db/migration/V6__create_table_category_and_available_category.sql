47a0ae0b34c54fa59fdc9bf53d3763623310bbea
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-08-05 13:23:02 +0900

    refactor: DB 변경사항으로 인한 flyway 쿼리문 작성

---
+++ backend/src/main/resources/db/migration/V6__create_table_category_and_available_category.sql
@@ -0,0 +1,15 @@
+CREATE TABLE category
+(
+    id          bigint NOT NULL PRIMARY KEY,
+    created_at datetime(6) NOT NULL,
+    modified_at datetime(6) NOT NULL,
+    content     varchar(255) NOT NULL
+)
+
+CREATE TABLE available_category (
+    id bigint NOT NULL PRIMARY KEY,
+    category_id bigint NOT NULL,
+    organization_id bigint NOT NULL,
+    CONSTRAINT FK_available_category_category_id FOREIGN KEY (category_id) REFERENCES category (id),
+    CONSTRAINT FK_available_category_organization_id FOREIGN KEY (organization_id) REFERENCES organization (id)
+)
---
+++ backend/src/main/resources/db/migration/V7__add_column_category_id_to_feedback.sql
@@ -0,0 +1,4 @@
+ALTER TABLE feedback ADD category_id bigint NOT NULL;
+ALTER TABLE feedback ADD CONSTRAINT FK_feedback_category_id FOREIGN KEY (category_id) REFERENCES category (id);
+
+
