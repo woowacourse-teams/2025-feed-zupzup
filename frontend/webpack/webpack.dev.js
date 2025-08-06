2f6f92074fc00854a86fbf53ac351c35497304a0
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-08-06 10:52:46 +0900

    Chore/#348 dev prod env (#349)
    * feat: 스타일 파일 통합 및 불필요한 파일 삭제
    
    * feat: customCSS 타입을 배열로 변경
    
    * feat: 피드백 제출 후 내 피드백 목록 업데이트 및 스타일 추가
    
    * feat: 피드백 박스에 사용자 이름에 '나' 표시 추가
    
    * feat: 사용자 피드백 박스에 '나' 표시 로직 개선 및 비밀글 처리 방식 수정
    
    * fix: 불필요한 공백 제거
    
    * feat: 피드백 객체의 타입을 상수로 변경하고 customCSS를 배열로 수정
    
    * refactor: useMyFeedbacks 훅의 함수 이름을 getIsMyFeedback으로 변경
    
    * feat: 환경 변수 설정을 위한 dotenv 추가 및 webpack 설정 수정
    
    * build: 변경된 빌드 명령어를 npm run build:dev로 수정
    
    * feat: CI 설정에서 개발 및 프로덕션 빌드 작업 추가

--- frontend/webpack/webpack.dev.js
+++ frontend/webpack/webpack.dev.js
@@ -1,18 +1,27 @@
-import { merge } from "webpack-merge";
-import common from "./webpack.common.js";
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
+
+const result = dotenv.config({ path: '.env.dev' });
+const env = result.parsed || {};
+const defineEnv = {
+  'process.env': JSON.stringify(env),
+};
 
 export default merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
     port: 3000,
     open: {
       app: {
-        name: "google chrome",
+        name: 'google chrome',
       },
     },
     hot: true,
     historyApiFallback: true,
   },
+  plugins: [new webpack.DefinePlugin(defineEnv)],
 });
