abfdc93e914a0b227eee8974236efa8bd7a61f41
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-08-06 12:16:11 +0900

    refactor: flyway에 더미데이터 삽입

--- backend/src/main/resources/db/migration/V8__add_column_organization_category_id_to_feedback.sql
+++ backend/src/main/resources/db/migration/V8__add_column_organization_category_id_to_feedback.sql
@@ -1,4 +1,4 @@
-ALTER TABLE feedback ADD COLUMN organization_category_id bigint;
+ALTER TABLE feedback ADD COLUMN organization_category_id bigint NOT NULL DEFAULT 1;
 
 ALTER TABLE feedback ADD CONSTRAINT FK_feedback_organization_category_id
     FOREIGN KEY (organization_category_id) REFERENCES organization_category (id);
