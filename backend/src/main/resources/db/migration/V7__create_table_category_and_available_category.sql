7ce868521f91c261b05bb372d691ecb10e931241
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-08-06 12:50:22 +0900

    refactor: 더미 데이터 순서 번경

--- backend/src/main/resources/db/migration/V7__create_table_category_and_available_category.sql
+++ backend/src/main/resources/db/migration/V7__create_table_category_and_available_category.sql
@@ -5,7 +5,7 @@ CREATE TABLE organization_category (
     CONSTRAINT FK_organization_category_organization_id FOREIGN KEY (organization_id) REFERENCES organization (id)
 );
 
-INSERT INTO organization_category values (1, 1, 'CURRICULUM');
+INSERT INTO organization_category values (1, 1, 'FACILITY');
 INSERT INTO organization_category values (2, 1, 'ADMINISTRATION');
 INSERT INTO organization_category values (3, 1, 'ETC');
-INSERT INTO organization_category values (4, 1, 'FACILITY');
+INSERT INTO organization_category values (4, 1, 'CURRICULUM');
