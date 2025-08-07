12580f7e7b21d1dd70eea26b86a2236e167070ed
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-08-07 14:30:01 +0900

    feat: cors origin 주소 추가

--- backend/src/main/java/feedzupzup/backend/global/config/CorsConfig.java
+++ backend/src/main/java/feedzupzup/backend/global/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://192.168.6.6:3000",
                         "https://feedzupzup.com"
                 )
-                .allowedOriginPatterns("https://*.feedzupzup.com")
+                .allowedOriginPatterns(
+                        "https://*.feedzupzup.com",
+                        "http://*.feedzupzup.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
