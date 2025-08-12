719caf1db5344f5d68ba11f7272f0b539e95c31b
yeongipark <yeongi0111@naver.com>
2025-08-12 15:43:45 +0900

    refactor: QRModal 컴포넌트 분리

--- frontend/src/domains/admin/components/QRModal/QRModal.stories.tsx
+++ frontend/src/domains/admin/components/QRModal/QRModal.stories.tsx
@@ -1,4 +1,4 @@
-import QRModal from '@/domains/admin/components/QRModal';
+import QRModal from '@/domains/admin/components/QRModal/QRModal';
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 const meta: Meta<typeof QRModal> = {
