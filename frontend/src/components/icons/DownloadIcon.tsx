cf1f3fb9f647c90144d343362fcf58c1eaae9a04
yeongipark <122340316+yeongipark@users.noreply.github.com>
2025-08-13 14:58:35 +0900

    [FE] feat: QR 페이지 구현 (#424)
    * chore: QR 모달에 필요한 아이콘 설정
    
    * feat: BasicButton 더 유연하게 사용할 수 있도록 props 수정
    
    * feat: QRModal 구현
    
    * refactor: QRModal 컴포넌트 분리
    
    * chore: console 제거
    
    * refactor: props url로 통일
    
    * chore: 안쓰는 props의 타입 삭제
    
    * refactor: QRModal 구조 개선 및 하위 컴포넌트 통합

---
+++ frontend/src/components/icons/DownloadIcon.tsx
@@ -0,0 +1,33 @@
+export default function DownloadIcon() {
+  return (
+    <svg
+      width='15'
+      height='14'
+      viewBox='0 0 15 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M7.75 8.75V1.75'
+        stroke='black'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M13 8.75V11.0833C13 11.3928 12.8771 11.6895 12.6583 11.9083C12.4395 12.1271 12.1428 12.25 11.8333 12.25H3.66667C3.35725 12.25 3.0605 12.1271 2.84171 11.9083C2.62292 11.6895 2.5 11.3928 2.5 11.0833V8.75'
+        stroke='black'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M4.8335 5.83301L7.75016 8.74967L10.6668 5.83301'
+        stroke='black'
+        strokeWidth='1.16667'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
