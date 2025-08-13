662e249e1738dc3d2431d3aa116523a722c1cdd4
bunju20 <ghkd2271@naver.com>
2025-08-13 12:41:00 +0900

    setting: 알림 기능 초기 세팅

---
+++ frontend/src/firebase/config.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { FirebaseOptions } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+if (
+  !process.env.FIREBASE_API_KEY ||
+  !process.env.FIREBASE_AUTH_DOMAIN ||
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_STORAGE_BUCKET ||
+  !process.env.FIREBASE_MESSAGING_SENDER_ID ||
+  !process.env.FIREBASE_APP_ID
+) {
+  throw new Error('Missing Firebase environment variables');
+}
+
+export const firebaseApp = initializeApp(firebaseConfig as FirebaseOptions);
+export const VAPID_KEY = process.env.FIREBASE_VAPID_KEY!;
