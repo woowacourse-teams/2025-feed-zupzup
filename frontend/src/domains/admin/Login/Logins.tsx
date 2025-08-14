0fdefa129c90efa6350e7104e108c6c80206a127
mun-kyeong <horse6953@naver.com>
2025-08-14 09:28:34 +0900

    refactor: 로그인 및 회원가입 컴포넌트 구조 변경 및 스타일 파일 이동

--- frontend/src/domains/admin/Login/Logins.tsx
+++ frontend/src/domains/admin/Login/Logins.tsx
@@ -5,12 +5,12 @@ import FormField from '@/domains/admin/components/FormField/FormField';
 import {
   LoginField,
   loginFields,
-} from '@/domains/admin/Logins/constants/loginFields';
+} from '@/domains/admin/Login/constants/loginFields';
 import {
   fieldContainer,
   loginCaptionContainer,
   loginForm,
-} from '@/domains/admin/Logins/Login.style';
+} from '@/domains/admin/Login/Login.style';
 import useAuthForm from '@/domains/admin/hooks/useAuthForm';
 import { useAppTheme } from '@/hooks/useAppTheme';
 import {
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,7 +1,7 @@
 import App from './App';
 import AdminDashboard from '@/domains/admin/adminDashboard/AdminDashboard';
-import Login from '@/domains/admin/Logins/Logins';
-import SignUp from '@/domains/admin/SignUps/SignUp';
+import Login from '@/domains/admin/Login/Logins';
+import SignUp from '@/domains/admin/SignUp/SignUp';
 import Home from '@/domains/Home';
 import UserDashboard from '@/domains/user/userDashboard/UserDashboard';
 import { createBrowserRouter } from 'react-router-dom';
