902b66ceed64477af55060ce1abbc044aae60f1f
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-08-14 11:21:57 +0900

    test: 조직 생성 service, E2E 테스트 코드 작성

---
+++ backend/src/test/java/feedzupzup/backend/organization/application/AdminOrganizationServiceTest.java
@@ -0,0 +1,73 @@
+package feedzupzup.backend.organization.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import feedzupzup.backend.admin.domain.Admin;
+import feedzupzup.backend.admin.domain.AdminRepository;
+import feedzupzup.backend.admin.domain.fixture.AdminFixture;
+import feedzupzup.backend.config.ServiceIntegrationHelper;
+import feedzupzup.backend.global.exception.ResourceException.ResourceNotFoundException;
+import feedzupzup.backend.organization.dto.request.CreateOrganizationRequest;
+import feedzupzup.backend.organization.dto.response.AdminCreateOrganizationResponse;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class AdminOrganizationServiceTest extends ServiceIntegrationHelper {
+
+    @Autowired
+    private AdminOrganizationService adminOrganizationService;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Test
+    @DisplayName("정상적인 admin이 조직을 생성하려고 할 때, 생성할 수 있어야 한다.")
+    void save_success_case() {
+        CreateOrganizationRequest createOrganizationRequest =
+                new CreateOrganizationRequest(
+                        "우테코",
+                        List.of("건의", "신고")
+                );
+
+        final Admin savedAdmin = createAndSaveAdmin();
+
+        final AdminCreateOrganizationResponse response = adminOrganizationService.saveOrganization(
+                createOrganizationRequest, savedAdmin.getId());
+
+        assertThat(response.organizationId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 admin이 조직을 생성하려고 한다면, 예외가 발생해야 한다.")
+    void invalid_admin_request_then_throw_exception() {
+        CreateOrganizationRequest createOrganizationRequest =
+                new CreateOrganizationRequest(
+                        "우테코",
+                        List.of("건의", "신고")
+                );
+
+        assertThatThrownBy(() -> adminOrganizationService.saveOrganization(createOrganizationRequest, 999L))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 카테고리를 생성하려고 한다면, 예외가 발생해야 한다.")
+    void invalid_category_then_throw_exception() {
+        CreateOrganizationRequest createOrganizationRequest =
+                new CreateOrganizationRequest(
+                        "우테코",
+                        List.of("크롱크롱", "대나무헬리콥터")
+                );
+        final Admin admin = createAndSaveAdmin();
+        assertThatThrownBy(() -> adminOrganizationService.saveOrganization(createOrganizationRequest, admin.getId()))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    private Admin createAndSaveAdmin() {
+        final Admin admin = AdminFixture.create();
+        return adminRepository.save(admin);
+    }
+}
---
+++ backend/src/test/java/feedzupzup/backend/organization/controller/AdminOrganizationControllerE2ETest.java
@@ -0,0 +1,78 @@
+package feedzupzup.backend.organization.controller;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+import feedzupzup.backend.admin.domain.Admin;
+import feedzupzup.backend.admin.domain.AdminRepository;
+import feedzupzup.backend.admin.domain.vo.AdminName;
+import feedzupzup.backend.admin.domain.vo.LoginId;
+import feedzupzup.backend.admin.domain.vo.Password;
+import feedzupzup.backend.auth.application.PasswordEncoder;
+import feedzupzup.backend.auth.dto.request.LoginRequest;
+import feedzupzup.backend.config.E2EHelper;
+import feedzupzup.backend.organization.dto.request.CreateOrganizationRequest;
+import io.restassured.http.ContentType;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+class AdminOrganizationControllerE2ETest extends E2EHelper {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("정상적인 조직 생성 요청시 조직 생성이 성공한다")
+    void createOrganization_Success() {
+        // given
+        final Password password = Password.createPassword("password123");
+        Admin admin = new Admin(new LoginId("testId"), passwordEncoder.encode(password), new AdminName("testName"));
+        adminRepository.save(admin);
+
+        LoginRequest loginRequest = new LoginRequest("testId", "password123");
+        String sessionCookie = given()
+                .contentType(ContentType.JSON)
+                .body(loginRequest)
+                .when()
+                .post("/admin/login")
+                .then()
+                .extract()
+                .cookie("JSESSIONID");
+
+        CreateOrganizationRequest request = new CreateOrganizationRequest("우아한테크코스", List.of("신고", "건의"));
+
+        // When & Then
+        given()
+                .contentType(ContentType.JSON)
+                .cookie("JSESSIONID", sessionCookie)
+                .body(request)
+        .when()
+                .post("/admin/organizations")
+        .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .log().all()
+                .body("data.organizationId", notNullValue());
+    }
+
+    @Test
+    @DisplayName("admin 권한 없이 요청이 올 경우, 권한 오류가 발생해야 한다.")
+    void not_authorization_case() {
+        // given
+        CreateOrganizationRequest request = new CreateOrganizationRequest("우아한테크코스", List.of("신고", "건의"));
+
+        // When & Then
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/admin/organizations")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+}
