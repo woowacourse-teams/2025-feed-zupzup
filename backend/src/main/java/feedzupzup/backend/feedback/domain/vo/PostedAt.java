acac3008b5568c31a2fef1c6cd2da5c235fbc40d
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-08-14 22:28:45 +0900

    [BE] feat: 조직 생성, 조회 API 구현 (#463)
    * rename: Role MENAGER -> MANAGER 으로 수정
    
    * refactor: Organizer 지연 로딩으로 수정
    
    * feat: 조직 생성 구현
    
    * refactor: 조직 생성 시 반환값 추가
    
    * test: 조직 생성 service, E2E 테스트 코드 작성
    
    * refactor: 비로그인시 400 -> 401 statusCode로 수정
    
    * feat: 관리자가 속한 조직 정보 조회 API 구현
    
    * test: 관리자가 속한 조직 조회 E2E 테스트코드 작성
    
    * [BE] refactor: EncodedPassword 객체 생성 (#455)
    
    * feat: EncodedPassword 생성
    
    * feat: final 키워드 추가
    
    * refactor: 컨벤션 수정
    
    * refactor: 컨벤션 수정
    
    * refactor: password 생성자 삭제로 인한 리팩토링
    
    * test: 순서에 따라 테스트 코드가 실패해, index에 상관없이 통과하도록 수정
    
    * refactor: OrganizationRepositoryTest 코드 수정 및 PostedAt get 메서드 네이밍 수정
    
    * refactor: 테스트가 실패하는 이유를 확인하기 위한 workflow file 수정
    
    * test: DB 저장 형식에 맞게 postedAt 형식 수정
    
    * refactor: postedAt 명시적으로 타입 설정
    
    * test: postedAt equals & hashcode 재정의
    
    * test: save 밖으로 분리
    
    * refactor: postedAt 6자리로 절삭
    
    * refactor: MAX_NAME 20자로 수정
    
    * refactor: 컨벤션 미적용 수정
    
    - final 키워드 적용
    - save -> create
    
    * refactor: CurrentDateTime 도입으로 인한 기존 코드 수정
    
    * refactor: organizationCategories 책임분리 및 반복문 add() 대신 addAll() 추가
    
    * test: SessionId 상수화
    
    ---------
    
    Co-authored-by: koseonje <kosunje1344@naver.com>

--- backend/src/main/java/feedzupzup/backend/feedback/domain/vo/PostedAt.java
+++ backend/src/main/java/feedzupzup/backend/feedback/domain/vo/PostedAt.java
@@ -2,7 +2,6 @@ package feedzupzup.backend.feedback.domain.vo;
 
 import jakarta.persistence.Embeddable;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -10,26 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostedAt {
 
-    private static final String TIME_ZONE_AREA = "Asia/Seoul";
-
     private LocalDateTime postedAt;
 
-    private PostedAt(final LocalDateTime postedAt) {
+    public PostedAt(final LocalDateTime postedAt) {
         this.postedAt = postedAt;
     }
 
-    public static PostedAt createTimeInSeoul() {
-        return new PostedAt(LocalDateTime.now(ZoneId.of(TIME_ZONE_AREA)));
-    }
-
-    /**
-     * 테스트 코드를 위한 생성자 입니다.
-     */
-    public static PostedAt from(LocalDateTime postedAt) {
-        return new PostedAt(postedAt);
-    }
-
-    public LocalDateTime getPostedAt() {
+    public LocalDateTime getValue() {
         return postedAt;
     }
 }
