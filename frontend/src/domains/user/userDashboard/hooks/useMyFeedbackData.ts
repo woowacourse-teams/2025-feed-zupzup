a25b8cd2d3bbc3bd9b61aa1969af925c3c9c4563
bunju20 <ghkd2271@naver.com>
2025-08-14 14:52:00 +0900

    chore: 로딩 및 에러처리 삭제

--- frontend/src/domains/user/userDashboard/hooks/useMyFeedbackData.ts
+++ frontend/src/domains/user/userDashboard/hooks/useMyFeedbackData.ts
@@ -5,36 +5,24 @@ import { getLocalStorage } from '@/utils/localStorage';
 
 export function useMyFeedbackData() {
   const [myFeedbacks, setMyFeedbacks] = useState<FeedbackType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchMyFeedbacks = async () => {
       const myFeedbackIds = getLocalStorage<number[]>('feedbackIds') || [];
       if (myFeedbackIds.length === 0) return;
 
-      setIsLoading(true);
-      setError(null);
+      const response = await getMyFeedbacks({
+        organizationId: 1,
+        feedbackIds: myFeedbackIds,
+      });
 
-      try {
-        const response = await getMyFeedbacks({
-          organizationId: 1,
-          feedbackIds: myFeedbackIds,
-        });
-
-        if (response?.data?.feedbacks) {
-          setMyFeedbacks(response.data.feedbacks);
-        }
-      } catch (err) {
-        console.error('내 피드백 초기 로드 중 오류:', err);
-        setError('피드백을 불러오는데 실패했습니다.');
-      } finally {
-        setIsLoading(false);
+      if (response?.data?.feedbacks) {
+        setMyFeedbacks(response.data.feedbacks);
       }
     };
 
     fetchMyFeedbacks();
   }, []);
 
-  return { myFeedbacks, isLoading, error };
+  return { myFeedbacks };
 }
