cfc562e7c39b83d41b40edde41ec5b8aafff9172
mun-kyeong <horse6953@naver.com>
2025-08-16 01:43:46 +0900

    feat: 홈 경로 접근 시 "/1/submit"으로 리다이렉트 추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,7 +4,7 @@ import Login from '@/domains/admin/Login/Logins';
 import SignUp from '@/domains/admin/SignUp/SignUp';
 import Home from '@/domains/Home';
 import UserDashboard from '@/domains/user/userDashboard/UserDashboard';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Settings from './domains/admin/Settings/Settings';
 import AdminHome from './domains/admin/AdminHome/AdminHome';
 import { ROUTES } from '@/constants/routes';
@@ -14,6 +14,10 @@ export const router = createBrowserRouter([
     path: ROUTES.HOME,
     element: <App />,
     children: [
+      {
+        index: true, // "/"에 접근 시
+        element: <Navigate to='/1/submit' replace />, // "/1/submit"로 리다이렉트 (임시 -> 나중에 삭제)
+      },
       {
         path: ROUTES.SUBMIT,
         element: <Home />,
