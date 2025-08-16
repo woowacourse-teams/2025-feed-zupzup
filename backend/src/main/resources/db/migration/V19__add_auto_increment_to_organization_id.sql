db6ab0342bc763bd01d461e8b0096c676abba063
Yeongjae Kim <sees77770@gmail.com>
2025-08-17 08:48:38 +0900

    [BE] fix: organization ddl 수정 (#502)
    * fix: organization DDL auto increment 추가
    
    * fix: organization DDL auto increment 추가 버전 변경
    
    * fix: 외래키 삭제 후 설정 변경

--- backend/src/main/resources/db/migration/V19__add_auto_increment_to_organization_id.sql
+++ backend/src/main/resources/db/migration/V19__add_auto_increment_to_organization_id.sql
@@ -1,1 +1,20 @@
+ALTER TABLE feedback DROP FOREIGN KEY fk_feedback_organization;
+ALTER TABLE organization_category DROP FOREIGN KEY FK_organization_category_organization_id;
+ALTER TABLE organizer DROP FOREIGN KEY fk_organizer_organization;
+ALTER TABLE qr DROP FOREIGN KEY fk_qr_organization;
+
 ALTER TABLE organization MODIFY COLUMN id BIGINT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE organization AUTO_INCREMENT = 2;
+
+ALTER TABLE feedback ADD CONSTRAINT fk_feedback_organization 
+    FOREIGN KEY (organization_id) REFERENCES organization(id);
+
+ALTER TABLE organization_category ADD CONSTRAINT FK_organization_category_organization_id 
+    FOREIGN KEY (organization_id) REFERENCES organization(id);
+
+ALTER TABLE organizer ADD CONSTRAINT fk_organizer_organization 
+    FOREIGN KEY (organization_id) REFERENCES organization(id) ON DELETE CASCADE;
+
+ALTER TABLE qr ADD CONSTRAINT fk_qr_organization 
+    FOREIGN KEY (organization_id) REFERENCES organization(id) ON DELETE CASCADE;
