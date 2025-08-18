ffe274874a2fd1a2cc23d83efd8545d744f57870
yeongipark <yeongi0111@naver.com>
2025-08-18 16:19:34 +0900

    feat: qr 가져오는 로직 연결

--- frontend/src/domains/admin/components/QRModal/hooks/useQRCode.ts
+++ frontend/src/domains/admin/components/QRModal/hooks/useQRCode.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useErrorModalContext } from '@/contexts/useErrorModal';
 import { getQRCode } from '@/apis/qr.api';
+import { useOrganizationId } from '@/domains/hooks/useOrganizationId';
 
 interface QRCodeData {
   imageUrl: string;
@@ -11,13 +12,14 @@ export const useQRCode = () => {
   const [data, setData] = useState<QRCodeData | null>(null);
   const [loading, setLoading] = useState(false);
   const { showErrorModal } = useErrorModalContext();
+  const { organizationId } = useOrganizationId();
 
   useEffect(() => {
     const fetchQRCode = async () => {
       setLoading(true);
 
       try {
-        const result = await getQRCode({ organizationId: '1' });
+        const result = await getQRCode({ organizationId });
         setData(result.data);
       } catch (err) {
         showErrorModal(err, 'QR 코드 조회 실패');
