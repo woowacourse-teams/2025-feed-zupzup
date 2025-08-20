fb707fbaa671f3fef4257723691a29f19e706b02
yeongipark <122340316+yeongipark@users.noreply.github.com>
2025-08-20 10:48:42 +0900

    [FE] feat: 방수정 api 연결 (#542)
    * feat: 방수정 api 연결
    
    * feat: 방 수정 모달에 알림 기능 추가 및 API 호출 개선
    
    * chore: 오타 수정
    
    * chore: 필요없는 ? 삭제
    
    * chore: 쿼리키 상수화
    
    * refactor: 필요없는 타입 단언 삭제
    
    * chore: 에러 메시지 구체화
    
    * feat: 에러 핸들링 로직 추가
    
    * refactor: 쿼리 키 상수화2
    
    * type: 타입 중복 제거

--- frontend/src/constants/queryKeys.ts
+++ frontend/src/constants/queryKeys.ts
@@ -2,5 +2,6 @@ export const QUERY_KEYS = {
   organizationStatistics: (id: string) => ['organizationStatistics', id],
   infiniteFeedbacks: ['infinity', 'feedbacks'],
   notificationSettings: () => ['notificationSettings'] as const,
+  organizationData: ['organizationData'],
   adminOrganizations: () => ['adminOrganizations'] as const,
 };
