715227ab28439df3de4782eb090275adf0f79ae4
yeongipark <yeongi0111@naver.com>
2025-08-21 20:30:35 +0900

    fix: 권한 없는 uuid로 접근했을 때 접근 제한

--- frontend/src/hooks/useApiErrorHandler.ts
+++ frontend/src/hooks/useApiErrorHandler.ts
@@ -8,10 +8,12 @@ export function useApiErrorHandler() {
   const { showErrorModal } = useErrorModalContext();
 
   const handleApiError = async (error: ApiError) => {
+    resetLocalStorage('auth');
+    goPath('/login');
     if (error.status === 401) {
-      resetLocalStorage('auth');
       showErrorModal(error, '로그인 권한 없음');
-      goPath('/login');
+    } else if (error.status === 403) {
+      showErrorModal(error, '권한없음');
     }
   };
 
