3a0311cb5177b53fd3571a4ad45784331c75eafd
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-08-21 14:37:30 +0900

    [BE] refactor: 알람 설정 변경 및 로그 추가 (#619)
    * refactor: 알람 설정 변경 및 로그 추가
    
    * chore: 관리자 알림 설정 수정

--- backend/src/test/java/feedzupzup/backend/notification/AlertsFilteringTest.java
+++ backend/src/test/java/feedzupzup/backend/notification/AlertsFilteringTest.java
@@ -19,32 +19,6 @@ class AlertsFilteringTest extends ServiceIntegrationHelper {
     private AdminRepository adminRepository;
 
     @Test
-    @DisplayName("알림 설정이 켜진 관리자만 필터링된다")
-    void filterOnlyAlertsEnabledAdmins() {
-        // given
-        Admin adminWithAlertsOn = createAndSaveAdmin("admin1", "login1");
-        Admin adminWithAlertsOff = createAndSaveAdmin("admin2", "login2");
-        adminWithAlertsOff.updateAlertsSetting(false);
-        Admin anotherAdminWithAlertsOn = createAndSaveAdmin("admin3", "login3");
-        
-        
-        // when - 관리자들을 직접 필터링 (실제로는 UserFeedbackService에서 Organizer를 통해 필터링)
-        List<Admin> admins = List.of(adminWithAlertsOn, adminWithAlertsOff, anotherAdminWithAlertsOn);
-        List<Long> filteredAdminIds = admins.stream()
-                .filter(Admin::isAlertsOn)
-                .map(Admin::getId)
-                .toList();
-        
-        // then - 알림이 켜진 관리자 2명만 필터링됨
-        assertThat(filteredAdminIds).hasSize(2);
-        assertThat(filteredAdminIds).containsExactlyInAnyOrder(
-                adminWithAlertsOn.getId(), 
-                anotherAdminWithAlertsOn.getId()
-        );
-        assertThat(filteredAdminIds).doesNotContain(adminWithAlertsOff.getId());
-    }
-
-    @Test
     @DisplayName("모든 관리자가 알림을 끈 경우 빈 목록이 반환된다")
     void emptyListWhenAllAlertsOff() {
         // given
