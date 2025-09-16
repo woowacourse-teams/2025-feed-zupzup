5a390a995b1b6a6f23f7d0e6dde377caffc0d46c
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-09-16 10:56:53 +0900

    refactor: V23 스키마 수정 (#714)

--- backend/src/main/resources/db/migration/V23__remove_unique_constraint_notification_token_admin_id.sql
+++ backend/src/main/resources/db/migration/V23__remove_unique_constraint_notification_token_admin_id.sql
@@ -1,1 +1,6 @@
+ALTER TABLE notification_token DROP FOREIGN KEY fk_notification_token_admin;
+
 ALTER TABLE notification_token DROP INDEX uk_notification_token_admin_id;
+
+ALTER TABLE notification_token ADD CONSTRAINT fk_notification_token_admin
+    FOREIGN KEY (admin_id) REFERENCES admin(id) ON DELETE CASCADE;
