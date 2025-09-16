ad06bf5de83c6315d829cb3c0822bf20dd953e9f
mun-kyeong <horse6953@naver.com>
2025-09-17 00:59:26 +0900

    refactor: useLogout 훅을 react-query를 사용하여 변경

--- frontend/src/domains/admin/Settings/hooks/useLogout.ts
+++ frontend/src/domains/admin/Settings/hooks/useLogout.ts
@@ -3,25 +3,25 @@ import { useErrorModalContext } from '@/contexts/useErrorModal';
 import useNavigation from '@/domains/hooks/useNavigation';
 import { resetLocalStorage } from '@/utils/localStorage';
 import { NotificationService } from '@/services/notificationService';
+import { useMutation } from '@tanstack/react-query';
 
 export function useLogout() {
   const { showErrorModal } = useErrorModalContext();
   const { goPath } = useNavigation();
 
-  const handleLogout = async () => {
-    try {
-      const response = await postAdminLogout();
-      NotificationService.removeToken();
-
-      if (response.status !== 200) {
-        throw new Error('로그아웃 실패');
-      }
+  const { mutate: handleLogout } = useMutation({
+    mutationFn: postAdminLogout,
+    onError: (e) => {
+      showErrorModal(e, '에러');
+    },
+    onSuccess: () => {
       resetLocalStorage('auth');
       goPath('/login');
-    } catch (e) {
-      showErrorModal(e, '에러');
-    }
-  };
+    },
+    onSettled: () => {
+      NotificationService.removeToken();
+    },
+  });
 
   return {
     handleLogout,
