54ba5d77039bb9e0cc3991484486b18547aa5e57
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-09-16 11:53:54 +0900

    refactor: V24 스키마 수정

--- backend/src/main/resources/db/migration/V24__refactor_notification_token_to_notification.sql
+++ backend/src/main/resources/db/migration/V24__refactor_notification_token_to_notification.sql
@@ -1,5 +1,2 @@
--- Rename notification_token table to notification and rename value column to token
-RENAME TABLE notification_token TO notification;
-
 ALTER TABLE notification
-CHANGE COLUMN value token VARCHAR(500) NOT NULL;
+    CHANGE COLUMN registration_token token VARCHAR(500) NOT NULL;
