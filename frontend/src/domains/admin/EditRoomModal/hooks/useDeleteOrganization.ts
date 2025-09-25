ed34d0359bb05d95963a8f9a656ddd4542a0c498
yeongipark <122340316+yeongipark@users.noreply.github.com>
2025-09-25 17:36:23 +0900

    fix: 삭제 시 캐시 업데이트 해주도록 설정 (#771)

--- frontend/src/domains/admin/EditRoomModal/hooks/useDeleteOrganization.ts
+++ frontend/src/domains/admin/EditRoomModal/hooks/useDeleteOrganization.ts
@@ -2,10 +2,16 @@ import { ApiError } from '@/apis/apiClient';
 import { deleteAdminOrganization } from '@/apis/adminOrganization.api';
 import { useOrganizationId } from '@/domains/hooks/useOrganizationId';
 import { useApiErrorHandler } from '@/hooks/useApiErrorHandler';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import useNavigation from '@/domains/hooks/useNavigation';
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import { AdminAuthData } from '@/types/adminAuth';
+import { getLocalStorage } from '@/utils/localStorage';
 
 export default function useDeleteOrganization() {
+  const adminName =
+    getLocalStorage<AdminAuthData>('auth')?.adminName || '관리자';
+  const queryClient = useQueryClient();
   const { organizationId } = useOrganizationId();
   const { handleApiError } = useApiErrorHandler();
   const { goPath } = useNavigation();
@@ -17,6 +23,9 @@ export default function useDeleteOrganization() {
       handleApiError(error as ApiError);
     },
     onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.adminOrganizations(adminName),
+      });
       goPath('/admin/home');
     },
   });
