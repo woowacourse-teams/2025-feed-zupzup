3f93d6fbc6bac1b8e8397e4120375b150292231e
bunju20 <ghkd2271@naver.com>
2025-10-01 15:36:25 +0900

    chore: 필요없는 주석 삭제

--- frontend/src/components/SEO/SEO.tsx
+++ frontend/src/components/SEO/SEO.tsx
@@ -15,25 +15,20 @@ export const SEO = ({
   ogImage = '/og/og-default-1200x630.png',
   ogUrl = window.location.href,
 }: SEOProps) => {
-  // "로그인" → "로그인 | 피드줍줍"
   const fullTitle = title === '피드줍줍' ? title : `${title} | 피드줍줍`;
 
   return (
     <Helmet>
-      {/* 브라우저 탭 제목 */}
       <title>{fullTitle}</title>
 
-      {/* 검색 엔진용 */}
       <meta name='description' content={description} />
       <meta name='keywords' content={keywords} />
 
-      {/* 카카오톡/페이스북 공유용 */}
       <meta property='og:title' content={fullTitle} />
       <meta property='og:description' content={description} />
       <meta property='og:image' content={ogImage} />
       <meta property='og:url' content={ogUrl} />
 
-      {/* 트위터 공유용 */}
       <meta name='twitter:title' content={fullTitle} />
       <meta name='twitter:description' content={description} />
       <meta name='twitter:image' content={ogImage} />
