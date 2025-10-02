eb00e74f5a4eefe56c637ad35da00c498f3d8c1c
Yeongjae Kim <sees77770@gmail.com>
2025-10-03 06:11:16 +0900

    refactor: s3 업로드용 presigned url 발급 로직 개선

--- backend/src/main/java/feedzupzup/backend/s3/dto/request/PresignedUrlRequest.java
+++ backend/src/main/java/feedzupzup/backend/s3/dto/request/PresignedUrlRequest.java
@@ -3,12 +3,10 @@ package feedzupzup.backend.s3.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record PresignedUrlRequest(
-        @NotNull(message = "객체 ID는 필수입니다")
-        Long objectId,
-        @NotNull(message = "객체 타입은 필수입니다")
-        String extension,
         @NotNull(message = "객체 디렉토리는 필수입니다")
-        String objectDir
+        String objectDir,
+        @NotNull(message = "객체 확장자 타입은 필수입니다")
+        String extension
 ) {
 
 }
