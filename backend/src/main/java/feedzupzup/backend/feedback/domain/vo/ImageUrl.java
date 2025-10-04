f11630f9ca9cac53e0a298e6250d1195a431e064
Yeongjae Kim <sees77770@gmail.com>
2025-10-04 21:50:56 +0900

    refactor: ImageUrl 정규표현식 상수화

--- backend/src/main/java/feedzupzup/backend/feedback/domain/vo/ImageUrl.java
+++ backend/src/main/java/feedzupzup/backend/feedback/domain/vo/ImageUrl.java
@@ -11,10 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageUrl {
 
+    private static final String QUERY_PARAMETER_DELIMITER = "\\?";
+
     @Column(name = "image_url")
     private String value;
 
     public ImageUrl(final String presignedUrl) {
-        this.value = presignedUrl.split("\\?")[0];
+        this.value = presignedUrl.split(QUERY_PARAMETER_DELIMITER)[0];
     }
 }
