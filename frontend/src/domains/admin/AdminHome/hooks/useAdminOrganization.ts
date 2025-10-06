687f8d1aedf392568a66585d0e7d5ed4b0490bba
yeongipark <yeongi0111@naver.com>
2025-10-06 23:13:11 +0900

    refactor: 에러 처리 로직 삭제

--- frontend/src/domains/admin/AdminHome/hooks/useAdminOrganization.ts
+++ frontend/src/domains/admin/AdminHome/hooks/useAdminOrganization.ts
@@ -2,11 +2,8 @@ import {
   AdminOrganizationType,
   getAdminOrganization,
 } from '@/apis/adminOrganization.api';
-import { ApiError } from '@/apis/apiClient';
 import { QUERY_KEYS } from '@/constants/queryKeys';
-import { useApiErrorHandler } from '@/hooks/useApiErrorHandler';
 import { useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
 
 interface UseAdminOrganizationParams {
   adminName: string;
@@ -15,14 +12,9 @@ interface UseAdminOrganizationParams {
 export default function useAdminOrganization({
   adminName,
 }: UseAdminOrganizationParams) {
-  const { handleApiError } = useApiErrorHandler();
-
-  const {
-    data: adminOrganizations = [],
-    isLoading,
-    isError,
-    error,
-  } = useQuery<AdminOrganizationType[]>({
+  const { data: adminOrganizations = [], isLoading } = useQuery<
+    AdminOrganizationType[]
+  >({
     queryKey: QUERY_KEYS.adminOrganizations(adminName),
     queryFn: async () => {
       const res = await getAdminOrganization();
@@ -32,12 +24,6 @@ export default function useAdminOrganization({
     staleTime: 5 * 60 * 1000,
     gcTime: 15 * 60 * 1000,
   });
-
-  useEffect(() => {
-    if (isError) {
-      handleApiError(error as ApiError);
-    }
-  }, [isError, handleApiError]);
 
   return { adminOrganizations, isLoading };
 }
