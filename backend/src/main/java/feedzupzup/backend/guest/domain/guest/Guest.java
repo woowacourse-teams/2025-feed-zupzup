0483f2b1597bc6f83a440709cffbb1060b7bd3c3
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-10-07 16:05:25 +0900

    feat: Guest, History 엔티티 객체 생성 및 flyway script 작성

---
+++ backend/src/main/java/feedzupzup/backend/guest/domain/guest/Guest.java
@@ -0,0 +1,36 @@
+package feedzupzup.backend.guest.domain.guest;
+
+import feedzupzup.backend.global.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Guest extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private UUID visitorUuid;
+
+    private LocalDateTime connectedTime;
+
+    public Guest(@NonNull final Long id, @NonNull final UUID visitorUuid, @NonNull final LocalDateTime connectedTime) {
+        this.id = id;
+        this.visitorUuid = visitorUuid;
+        this.connectedTime = connectedTime;
+    }
+
+}
