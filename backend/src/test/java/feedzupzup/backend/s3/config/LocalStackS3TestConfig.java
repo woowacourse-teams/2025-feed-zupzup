1375265c8c4f34a05dc9aeaadf0277ae9be5d003
Yeongjae Kim <sees77770@gmail.com>
2025-10-09 00:56:42 +0900

    chore: LocalStack 테스트 환경 빈 구성

---
+++ backend/src/test/java/feedzupzup/backend/s3/config/LocalStackS3TestConfig.java
@@ -0,0 +1,62 @@
+package feedzupzup.backend.s3.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
+import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@TestConfiguration
+public class LocalStackS3TestConfig {
+
+    private static final DockerImageName LOCALSTACK_IMAGE =
+            DockerImageName.parse("localstack/localstack:3.0");
+
+    @Bean
+    static LocalStackContainer localStackContainer() {
+        return new LocalStackContainer(LOCALSTACK_IMAGE)
+                .withServices(Service.S3)
+                .withReuse(true);
+    }
+
+    @Bean
+    @Primary
+    public S3Client testS3Client(final LocalStackContainer localStackContainer) {
+        return S3Client.builder()
+                .endpointOverride(localStackContainer.getEndpointOverride(Service.S3))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        localStackContainer.getAccessKey(),
+                                        localStackContainer.getSecretKey()
+                                )
+                        )
+                )
+                .region(Region.of(localStackContainer.getRegion()))
+                .forcePathStyle(true)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public S3Presigner testS3Presigner(final LocalStackContainer localStackContainer) {
+        return S3Presigner.builder()
+                .endpointOverride(localStackContainer.getEndpointOverride(Service.S3))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        localStackContainer.getAccessKey(),
+                                        localStackContainer.getSecretKey()
+                                )
+                        )
+                )
+                .region(Region.of(localStackContainer.getRegion()))
+                .build();
+    }
+}
