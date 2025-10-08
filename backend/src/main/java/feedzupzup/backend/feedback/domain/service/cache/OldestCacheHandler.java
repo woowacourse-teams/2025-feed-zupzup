b4f79a2a28a9790f9310011a21ee7b35708bfd2e
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-10-08 15:15:30 +0900

    [BE] fix: 캐시 버그 수정 (#817)
    * refactor: 피드백 좋아요, 피드백 상태 변경 시 다른 곳에 캐시되어 있다면 전부 삭제
    
    * test: 피드백 변경 시 다른 캐시 반영 테스트 코드 작성

--- backend/src/main/java/feedzupzup/backend/feedback/domain/service/cache/OldestCacheHandler.java
+++ backend/src/main/java/feedzupzup/backend/feedback/domain/service/cache/OldestCacheHandler.java
@@ -1,10 +1,6 @@
 package feedzupzup.backend.feedback.domain.service.cache;
 
-import static feedzupzup.backend.global.domain.CacheType.*;
-
 import feedzupzup.backend.feedback.dto.response.FeedbackItem;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,23 +9,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OldestCacheHandler implements FeedbackCacheHandler {
 
-    private final CacheHelper cacheHelper;
+    private final FeedbackCacheRemover feedbackCacheRemover;
 
     @Override
     public void handle(final FeedbackItem savedFeedbackItem, final UUID organizationUuid) {
-        final Optional<List<FeedbackItem>> findCachedFeedbacks = cacheHelper.getCacheValueList(
-                organizationUuid, OLDEST_FEEDBACK.getCacheName());
-        if (findCachedFeedbacks.isEmpty()) {
-            return;
-        }
-
-        final List<FeedbackItem> feedbackItems = findCachedFeedbacks.get();
-
-        final boolean alreadyCached = feedbackItems.stream()
-                .anyMatch(feedbackItem -> feedbackItem.feedbackId().equals(savedFeedbackItem.feedbackId()));
-
-        if (alreadyCached) {
-            cacheHelper.removeCache(organizationUuid, OLDEST_FEEDBACK.getCacheName());
-        }
+        feedbackCacheRemover.removeAllFeedbackCachesInfContains(savedFeedbackItem, organizationUuid);
     }
 }
