52d3197ac10269d91d8a9d576e340d5f83c8df04
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-10-13 16:28:36 +0900

    refactor: flyway script 컬럼 수정 (#836)

--- backend/src/main/resources/db/migration/V32__add_table_write_history.sql
+++ backend/src/main/resources/db/migration/V32__add_table_write_history.sql
@@ -4,7 +4,7 @@ CREATE TABLE write_history
     guest_id    BIGINT NOT NULL,
     feedback_id BIGINT NOT NULL,
     created_at  DATETIME(6) NOT NULL,
-    updated_at  DATETIME(6) NOT NULL,
+    modified_at  DATETIME(6) NOT NULL,
     CONSTRAINT fk_write_history_guest FOREIGN KEY (guest_id) REFERENCES guest (id),
     CONSTRAINT fk_write_history_feedback FOREIGN KEY (feedback_id) REFERENCES feedback (id)
 );
