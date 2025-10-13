ff175caa6ec752de1880b998d4067cf6449bc932
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-10-13 16:20:57 +0900

    refactor: flyway script 컬럼 수정

--- backend/src/main/resources/db/migration/V30__add_table_guest.sql
+++ backend/src/main/resources/db/migration/V30__add_table_guest.sql
@@ -2,7 +2,7 @@ CREATE TABLE guest
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
     guest_uuid   BINARY(16) NOT NULL UNIQUE,
-    connected_time DATETIME(6),
+    connected_time DATETIME(6) NOT NULL,
     created_at     DATETIME(6) NOT NULL,
-    updated_at     DATETIME(6) NOT NULL
+    modified_at     DATETIME(6) NOT NULL
 );
--- backend/src/main/resources/db/migration/V31__add_table_like_history.sql
+++ backend/src/main/resources/db/migration/V31__add_table_like_history.sql
@@ -4,7 +4,7 @@ CREATE TABLE like_history
     guest_id    BIGINT NOT NULL,
     feedback_id BIGINT NOT NULL,
     created_at  DATETIME(6) NOT NULL,
-    updated_at  DATETIME(6) NOT NULL,
+    modified_at  DATETIME(6) NOT NULL,
     CONSTRAINT fk_like_history_guest FOREIGN KEY (guest_id) REFERENCES guest (id),
     CONSTRAINT fk_like_history_feedback FOREIGN KEY (feedback_id) REFERENCES feedback (id)
 );
