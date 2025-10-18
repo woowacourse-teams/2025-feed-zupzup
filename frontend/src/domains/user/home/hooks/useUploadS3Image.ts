0c3c34eb9b04441305f6a6c3ab38c454264d306b
mun-kyeong <horse6953@naver.com>
2025-10-18 20:39:53 +0900

    refactor: UploadS3ImageParams 인터페이스 추가

--- frontend/src/domains/user/home/hooks/useUploadS3Image.ts
+++ frontend/src/domains/user/home/hooks/useUploadS3Image.ts
@@ -3,19 +3,17 @@ import { uploadImageToS3 } from '@/apis/s3.api';
 import { useApiErrorHandler } from '@/hooks/useApiErrorHandler';
 import { useMutation } from '@tanstack/react-query';
 
+interface UploadS3ImageParams {
+  presignedUrl: string;
+  file: File;
+  contentType: string;
+}
+
 export function useUploadS3Image() {
   const { handleApiError } = useApiErrorHandler();
 
   const { mutateAsync: uploadS3PreSignUrl } = useMutation({
-    mutationFn: ({
-      presignedUrl,
-      file,
-      contentType,
-    }: {
-      presignedUrl: string;
-      file: File;
-      contentType: string;
-    }) =>
+    mutationFn: ({ presignedUrl, file, contentType }: UploadS3ImageParams) =>
       uploadImageToS3({
         presignedUrl,
         file,
