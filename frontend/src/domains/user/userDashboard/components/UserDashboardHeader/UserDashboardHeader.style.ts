921dc5764007f9e8523fb51f1e694b3678e55887
bunju20 <ghkd2271@naver.com>
2025-10-20 00:09:47 +0900

    feat: 유저 대시보드에 헤더 추가

---
+++ frontend/src/domains/user/userDashboard/components/UserDashboardHeader/UserDashboardHeader.style.ts
@@ -0,0 +1,36 @@
+import { Theme } from '@/theme';
+import { css } from '@emotion/react';
+
+export const headerLayout = (backgroundImage: string) => css`
+  position: fixed;
+  top: 0;
+  left: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  max-width: 600px;
+  padding: 20px 30px;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  transform: translateX(-50%);
+`;
+
+export const logoContainer = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const basketIcon = css`
+  width: 24px;
+  height: 24px;
+`;
+
+export const logoText = (theme: Theme) => css`
+  ${theme.typography.pretendard.captionBold};
+  color: ${theme.colors.white[100]};
+  letter-spacing: 0.1em;
+`;
---
+++ frontend/src/domains/user/userDashboard/components/UserDashboardHeader/UserDashboardHeader.tsx
@@ -0,0 +1,35 @@
+import {
+  headerLayout,
+  logoContainer,
+  basketIcon,
+  logoText,
+} from '@/domains/user/userDashboard/components/UserDashboardHeader/UserDashboardHeader.style';
+import { useAppTheme } from '@/hooks/useAppTheme';
+import basketImage from '@/assets/images/basket.png';
+import backgroundImage from '@/assets/images/background.png';
+import useNavigation from '@/domains/hooks/useNavigation';
+import { Analytics, userDashboardEvents } from '@/analytics';
+
+export default function UserDashboardHeader() {
+  const theme = useAppTheme();
+
+  const { goPath } = useNavigation();
+
+  const handleNavigateToOnboarding = () => {
+    Analytics.track(userDashboardEvents.viewSuggestionsFromDashboard());
+    goPath('/');
+  };
+
+  return (
+    <div css={headerLayout(backgroundImage)}>
+      <div
+        css={logoContainer}
+        onClick={handleNavigateToOnboarding}
+        style={{ cursor: 'pointer' }}
+      >
+        <img src={basketImage} alt='basket' css={basketIcon} />
+        <p css={logoText(theme)}>FEEDZUPZUP</p>
+      </div>
+    </div>
+  );
+}
