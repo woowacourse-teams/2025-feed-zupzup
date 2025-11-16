aae04d7cd9a4353ba14f77d66d5e365acde7d06f
Yeongjae Kim <sees77770@gmail.com>
2025-11-16 17:36:00 +0900

    fix: 어노테이션 추가

--- backend/src/main/java/feedzupzup/backend/feedback/application/FeedbackFileDownloadService.java
+++ backend/src/main/java/feedzupzup/backend/feedback/application/FeedbackFileDownloadService.java
@@ -16,10 +16,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class FeedbackFileDownloadService {
 
     private final FeedbackDownloadJobStore feedbackDownloadJobStore;
