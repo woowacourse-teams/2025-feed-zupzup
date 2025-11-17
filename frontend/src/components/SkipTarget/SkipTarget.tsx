b572537db46ee97a5fd143b2419910dd5fc20119
mun-kyeong <horse6953@naver.com>
2025-11-17 16:16:41 +0900

    SkipTarget 컴포넌트에서 대체 타겟을 찾기 위한 setTimeout 제거

--- frontend/src/components/SkipTarget/SkipTarget.tsx
+++ frontend/src/components/SkipTarget/SkipTarget.tsx
@@ -10,13 +10,6 @@ export default function SkipTarget({ targetId }: { targetId: string }) {
           el.setAttribute('tabindex', '-1');
           el.focus({ preventScroll: true });
           el.scrollIntoView({ block: 'start', behavior: 'smooth' });
-        } else {
-          setTimeout(() => {
-            const target = document.getElementById(targetId);
-            target?.setAttribute('tabindex', '-1');
-            target?.focus({ preventScroll: true });
-            target?.scrollIntoView({ block: 'start', behavior: 'smooth' });
-          }, 100);
         }
       }}
     >
