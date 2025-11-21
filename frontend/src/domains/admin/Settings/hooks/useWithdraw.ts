3796a7f97a06a0c48c44ac65aae9b88603372d3d
bunju20 <ghkd2271@naver.com>
2025-11-21 11:39:38 +0900

    feat: 에러처리 추가

--- frontend/src/domains/admin/Settings/hooks/useWithdraw.ts
+++ frontend/src/domains/admin/Settings/hooks/useWithdraw.ts
@@ -3,18 +3,21 @@ import useNavigation from '@/domains/hooks/useNavigation';
 import { NotificationService } from '@/services/notificationService';
 import { resetLocalStorage } from '@/utils/localStorage';
 import { useMutation } from '@tanstack/react-query';
+import { useToast } from '@/contexts/useToast';
 
 export function useWithdraw() {
   const { goPath } = useNavigation();
+  const { showToast } = useToast();
 
   const { mutateAsync: handleWithdraw } = useMutation({
     mutationFn: deleteAdmin,
     onSuccess: () => {
       resetLocalStorage('auth');
+      NotificationService.removeToken();
       goPath('/');
     },
-    onSettled: () => {
-      NotificationService.removeToken();
+    onError: () => {
+      showToast('탈퇴 처리 중 문제가 발생했어요. 잠시 후 다시 시도해주세요');
     },
   });
 
