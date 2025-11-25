9bf1a8ea7260492b2441df87e89b3efe1e67e389
bunju20 <ghkd2271@naver.com>
2025-11-25 15:07:27 +0900

    fix: 에러처리 구문 삭제

--- frontend/src/domains/admin/Settings/hooks/useWithdraw.ts
+++ frontend/src/domains/admin/Settings/hooks/useWithdraw.ts
@@ -3,11 +3,9 @@ import useNavigation from '@/domains/hooks/useNavigation';
 import { NotificationService } from '@/services/notificationService';
 import { resetLocalStorage } from '@/utils/localStorage';
 import { useMutation } from '@tanstack/react-query';
-import { useToast } from '@/contexts/useToast';
 
 export function useWithdraw() {
   const { goPath } = useNavigation();
-  const { showToast } = useToast();
 
   const { mutateAsync: handleWithdraw } = useMutation({
     mutationFn: deleteAdmin,
@@ -15,9 +13,6 @@ export function useWithdraw() {
       resetLocalStorage('auth');
       NotificationService.removeToken();
       goPath('/');
-    },
-    onError: () => {
-      showToast('탈퇴 처리 중 문제가 발생했어요. 잠시 후 다시 시도해주세요');
     },
   });
 
