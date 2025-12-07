0657e4bd613f673fdb47add1aef2cfe3f600c6ed
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-12-08 01:46:16 +0900

    refactor: 미사용 import문 제거

--- backend/src/main/java/feedzupzup/backend/feedback/application/event/OrganizationFeedbackCountHandler.java
+++ backend/src/main/java/feedzupzup/backend/feedback/application/event/OrganizationFeedbackCountHandler.java
@@ -1,10 +1,8 @@
 package feedzupzup.backend.feedback.application.event;
 
 import feedzupzup.backend.feedback.event.OrganizationFeedbackCountEvent;
-import feedzupzup.backend.organization.application.OrganizationStatisticService;
 import feedzupzup.backend.organization.domain.FeedbackAmount;
 import feedzupzup.backend.organization.domain.OrganizationStatisticRepository;
-import feedzupzup.backend.organization.dto.response.OrganizationStatisticResponse;
 import feedzupzup.backend.sse.service.SseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
