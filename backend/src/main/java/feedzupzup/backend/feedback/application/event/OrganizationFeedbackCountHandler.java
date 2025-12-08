7c903c463fc0859d1cbc19d085e357d31c39df0b
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-12-08 22:38:26 +0900

    refactor: 실시간 피드백 알림 비동기 이벤트 예외처리 추가

--- backend/src/main/java/feedzupzup/backend/feedback/application/event/OrganizationFeedbackCountHandler.java
+++ backend/src/main/java/feedzupzup/backend/feedback/application/event/OrganizationFeedbackCountHandler.java
@@ -5,6 +5,7 @@ import feedzupzup.backend.organization.domain.FeedbackAmount;
 import feedzupzup.backend.organization.domain.OrganizationStatisticRepository;
 import feedzupzup.backend.sse.service.SseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -12,6 +13,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OrganizationFeedbackCountHandler {
 
     private final OrganizationStatisticRepository organizationStatisticRepository;
@@ -20,12 +22,16 @@ public class OrganizationFeedbackCountHandler {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Async
     public void handleFeedbackCreatedEvent(final OrganizationFeedbackCountEvent organizationFeedbackCountEvent) {
-        final FeedbackAmount feedbackAmount = organizationStatisticRepository.findFeedbackAmountByOrganizationId(
-                organizationFeedbackCountEvent.organizationId());
+        try {
+            final FeedbackAmount feedbackAmount = organizationStatisticRepository.findFeedbackAmountByOrganizationId(
+                    organizationFeedbackCountEvent.organizationId());
 
-        sseService.sendFeedbackNotificationToOrganization(
-                organizationFeedbackCountEvent.organizationId(),
-                feedbackAmount.getFeedbackTotalCount()
-        );
+            sseService.sendFeedbackNotificationToOrganization(
+                    organizationFeedbackCountEvent.organizationId(),
+                    feedbackAmount.getFeedbackTotalCount()
+            );
+        } catch (Exception e) {
+            log.error("피드백 실시간 알림 비동기 처리 중 예외 발생 - OrganizationId : {}", organizationFeedbackCountEvent.organizationId(), e);
+        }
     }
 }
