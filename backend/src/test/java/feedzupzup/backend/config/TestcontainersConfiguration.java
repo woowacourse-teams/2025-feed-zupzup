8a2415ad4aa8834942e1edf0c1b1ec4d82d8dc2f
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-12-14 00:45:46 +0900

    refactor: redis 테스트 컨테이너 수정

--- backend/src/test/java/feedzupzup/backend/config/TestcontainersConfiguration.java
+++ backend/src/test/java/feedzupzup/backend/config/TestcontainersConfiguration.java
@@ -1,10 +1,10 @@
 package feedzupzup.backend.config;
 
-import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,9 +13,10 @@ import org.testcontainers.utility.DockerImageName;
 public class TestcontainersConfiguration {
 
     @Bean
-    @ServiceConnection
-    static RedisContainer redisContainer() {
-        return new RedisContainer(DockerImageName.parse("redis:7-alpine"))
+    @ServiceConnection(name = "redis")
+    static GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+            .withExposedPorts(6379)
             .withReuse(true);
     }
 
